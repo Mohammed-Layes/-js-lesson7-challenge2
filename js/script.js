30da9c4aaba72a9de0bead933138bbe1c0ab7568
Mohammed.Layes <mohammed.layes@gmail.com>
2023-03-15 16:53:40 -0400

    Added logic to subscription calculator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 // Your JS code here
+var subTypeElement = document.querySelector("select#subscription");
+
+var subDurationElement = document.querySelector("select#months");
+
+var result = document.querySelector("p.result");
+
+var subType = "basic";
+
+var subDuration = 1;
+
+subTypeElement.addEventListener("change", function(e) {
+    subType = e.target.value;
+    updateSubscriptionDiv();
+    //console.log(subType);
+});
+
+subDurationElement.addEventListener("change", function(e) {
+    subDuration = Number(e.target.value);
+    updateSubscriptionDiv();
+    //console.log(subDuration);
+});
+
+var updateSubscriptionDiv = function() {
+    monthlyCost = 5;
+    if (subType === "standard") {
+        monthlyCost = 7;
+    } else if (subType === "premium") {
+        monthlyCost = 10;
+    }
+    
+    var total = subDuration * monthlyCost;
+
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
